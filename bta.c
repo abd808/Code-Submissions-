b5740d28e94647e2c405dd5cd8c7b9e12c970b1c
abd808 <57211806+abd808@users.noreply.github.com>
2019-10-31 13:40:48 +0530

    Solution for BTA prob

---
+++ bta.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<string.h>
+
+long long int c[100005];
+char s[100005],a[2],x;
+
+int main()
+{
+    long long int t,n,k,z,min;
+    scanf("%lld",&t);
+    while(t)
+    {
+        t--;
+        k=0;
+        z=strlen(s);
+        min=100010;
+        scanf("%s %s %lld",s,a,&n); 
+        x=a[0];
+        for(int i=0;i<z;i++)
+            c[i]=0;
+        for(int i=0;s[i]!='\0';i++)
+            if(s[i]==x)
+                c[k++]=i;
+        for(int i=0;i<(k-n+1);i++)
+            if(min>(c[i+n-1]-c[i]+1))
+                min=(c[i+n-1]-c[i]+1);
+        printf("%lld\n",min);
+    }
+}
